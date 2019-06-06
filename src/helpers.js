e358c83ddaaf2e6012f98094e243516f00b60dde
Maxim Razhev <svognev@yandex.ru>
2019-06-06 19:35:10 +0300

    change routes paths

--- src/helpers.js
+++ src/helpers.js
@@ -8,9 +8,9 @@ export const replaceLangLinks = (id) => {
   const linksArray = document.getElementsByClassName(`changeLangLink`);
   for (let i = 0; i < linksArray.length; i++) {
     if (linksArray[i].href.includes("/de")) {
-      linksArray[i].href = `${window.PUBLIC_URL}/de/${id}`;
+      linksArray[i].href = `${window.PUBLIC_URL}/#/de/${id}`;
     } else {
-      linksArray[i].href = `${window.PUBLIC_URL}/en/${id}`;
+      linksArray[i].href = `${window.PUBLIC_URL}/#/en/${id}`;
     }
   }
 }
