d7441d41eef85ebf1ec2e3d0e7a47ed7bb70bc6d
Maxim Razhev <svognev@yandex.ru>
2019-06-20 18:59:07 +0300

    show correct patchnumber

--- src/components/version-heading-block/version-heading-block.js
+++ src/components/version-heading-block/version-heading-block.js
@@ -4,11 +4,12 @@ import "./version-heading-block.css";
 const VersionHeadingBlock = ({lang, versionNumber}) => {
     const headingText = lang === "en" ? "New in " : "Neu in ";
     const patchNumber = versionNumber !== "2.09" ? ".0" : ".7";
+    console.log(patchNumber);
     return (
         <div className="VersionHeadingBlock">
             <div className="VersionHeading">
                 <span className="VersionHeading-Text VersionHeading-Text_desktop">
-                    {versionNumber + ".0"}
+                    {versionNumber + patchNumber}
                 </span>
                 <span className="VersionHeading-Text VersionHeading-Text_mobile">
                     {headingText}{versionNumber + patchNumber}
